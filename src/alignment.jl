53b2638ee7cd4a6255aa72c05b59bf64da1154f2
aronStalmarck <aron.stalmarck@gmail.com>
2023-07-06 13:35:00 +0200

    copied affine alignment from NextGenSeqUtils, instead of dependency

--- src/alignment.jl
+++ src/alignment.jl
@@ -21,7 +21,7 @@ struct Alignment
     alignedtarget::String    
 end
 
-Alignment(; query::String, target::String) = Alignment(query, target, yeet_all_edges(NextGenSeqUtils.affine_nw_align(query, target)...)...)
+Alignment(; query::String, target::String) = Alignment(query, target, yeet_all_edges(NextGenSeqUtils_affine_nw_align(query, target)...)...)
 
 nummatches(alignment::Alignment) = count(x -> x[1] == x[2], zip(alignment.alignedquery, alignment.alignedtarget))
 
@@ -65,7 +65,7 @@ end
 alignabletargetinstances(targets::Vector{UnprocessedComplex}, query::ProcessedComplex; specialchain_minmatch::Real = 0.3, otherchains_minmatch::Real = 0.3) = vcat(Vector{ProcessedComplex}[alignabletargetinstances(target, query, specialchain_minmatch = specialchain_minmatch, otherchains_minmatch = otherchains_minmatch) for target in targets]...)
 
 function matchedindices(queryseq::String, targetseq::String)
-    query, target = NextGenSeqUtils.affine_nw_align(queryseq, targetseq) # We don't want to use the yeet_edges function here because we want to index into the original sequences, not the aligned ones
+    query, target = NextGenSeqUtils_affine_nw_align(queryseq, targetseq) # We don't want to use the yeet_edges function here because we want to index into the original sequences, not the aligned ones
 
     queryindices = Int[]
     targetindices = Int[]
@@ -121,4 +121,110 @@ function matchedcoords(query::Vector{ProteinChain}, target::Vector{ProteinChain}
     targetcoords = joincoords(target)[:, targetindices]
 
     return querycoords, targetcoords
+end
+
+
+
+
+
+function NextGenSeqUtils_affine_nw_align(s1::String, s2::String;
+    gap_open = -2.0,
+    gap_extend = -0.2,
+    match_cost = 1.0,
+    mismatch_cost = -1.0)
+   
+    s1arr = collect(s1)  # vertical
+    s1len = length(s1arr)
+    s2arr = collect(s2)  # horizontal
+    s2len = length(s2arr)
+  
+    M = zeros(s1len+1, s2len+1)
+    IX = zeros(s1len+1, s2len+1)
+    IY = zeros(s1len+1, s2len+1)
+
+    IX[:,1] .= gap_open .+ gap_extend * (0:s1len)
+    IX[1,:] .= -Inf * [1:s2len+1;]
+    IY[1,:] .= gap_open .+ gap_extend * (0:s2len)
+    IY[:,1] .= -Inf * [1:s1len+1;]
+    M[1:end,1] .= gap_open .+ gap_extend * (0:s1len) #Not sure about this.
+    M[1,1:end] .= gap_open .+ gap_extend * (0:s2len) #Not sure about this.
+    
+    traceM = zeros(Int, s1len+1, s2len+1)
+    traceIX = zeros(Int, s1len+1, s2len+1)
+    traceIY = zeros(Int, s1len+1, s2len+1)
+    
+    #ALSO UNSURE
+    traceM[1,:] .+= 3 
+    traceM[2:end,1] .+= 2
+    
+    traceIX[1,:] .+= 3 
+    traceIY[2:end,1] .+= 2
+    
+    traceIY[1,:] .+= 3 
+    traceIX[2:end,1] .+= 2
+    
+    for i in 2:s1len+1
+        for j in 2:s2len+1
+            diag_cost = 0.0
+            if s1arr[i-1] == s2arr[j-1]
+                diag_cost = match_cost
+            else
+                diag_cost = mismatch_cost
+            end
+            diagM = M[i-1, j-1] + diag_cost
+            IX2M = IX[i-1, j-1] + diag_cost
+            IY2M = IY[i-1, j-1] + diag_cost
+            
+            M[i,j],traceM[i,j] = findmax([diagM,IX2M,IY2M]) #1: stay in M. 2: come from IX. 3: come from IY
+            
+            M2IX = M[i-1, j] + gap_open
+            IXextend = IX[i-1, j] + gap_extend
+            IX[i,j],traceIX[i,j] = findmax([M2IX,IXextend]) #1: gap open. 2: gap extend
+            
+            M2IY = M[i, j-1] + gap_open
+            IYextend = IY[i, j-1] + gap_extend
+            IY[i,j],traceIY[i,j] = findmax([M2IY,-Inf,IYextend]) #1: gap open. 3: gap extend
+              
+        end
+    end
+    
+    rev_arr1 = Char[]
+    rev_arr2 = Char[]
+    sizehint!(rev_arr1, s1len+s2len)
+    sizehint!(rev_arr2, s1len+s2len)
+    
+    mats = [traceM,traceIX,traceIY]
+    x_i = s1len+1
+    y_i = s2len+1
+    m_i = argmax([M[x_i,y_i],IX[x_i,y_i],IY[x_i,y_i]])
+    while x_i-1 > 0 &&  y_i-1 > 0
+        next_m_i = mats[m_i][x_i,y_i]
+        if m_i == 1
+            push!(rev_arr1,s1arr[x_i-1])
+            push!(rev_arr2,s2arr[y_i-1])
+            
+            x_i -= 1
+            y_i -= 1
+        elseif m_i == 2
+            push!(rev_arr1,s1arr[x_i-1])
+            push!(rev_arr2,'-')
+            x_i -= 1
+        elseif m_i == 3
+            push!(rev_arr1,'-')
+            push!(rev_arr2,s2arr[y_i-1])
+            y_i -= 1
+        end
+        m_i = next_m_i
+    end
+    while x_i-1 > 0
+        push!(rev_arr1,s1arr[x_i-1])
+        push!(rev_arr2,'-')
+        x_i -= 1
+    end
+    while y_i-1 > 0
+        push!(rev_arr1,'-')
+        push!(rev_arr2,s2arr[y_i-1])
+        y_i -= 1
+    end
+    return join(reverse(rev_arr1)),join(reverse(rev_arr2))
 end
